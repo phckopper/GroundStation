ae2d10ee6f4c81fa17f539f4f184b96eee953a15
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2018-02-20 17:42:16 -0300

    Start using PhotonKit

--- main.js
+++ main.js
@@ -8,7 +8,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden',
+    frame: false,
+    kiosk: true
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -38,9 +44,8 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
+
 })
 
 app.on('activate', () => {
